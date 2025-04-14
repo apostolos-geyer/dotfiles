c204dd61d1bfa933208c47193e8af6e0ea4c521d
Apostoli Geyer <ajgeyer@uwaterloo.ca>
2025-04-13 21:51:34 -0400

    add ghostty config, remove ftplugins

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,19 +1,22 @@
+--- options
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.o.clipboard = "unnamedplus"
+vim.opt.timeout = true
+vim.opt.timeoutlen = 250
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.o.clipboard = "unnamedplus"
 
 --- SIDE EFFECTS: EVALUATES PROFILE, LOADS LAZY PLUGINS BASED ON PROFILE
 local config = require("configure")
 
 do
-  local keymapfns = config.profiles.eval(require("keymap"))
-  for i = 1, #keymapfns do
-    local cb = keymapfns[i]
-    cb()
-  end
+    local keymapfns = config.profiles.eval(require("keymap"))
+    for i = 1, #keymapfns do
+        local cb = keymapfns[i]
+        cb()
+    end
 end
